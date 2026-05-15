3278f2921090a8549029154b21715748b0a30bc2
Marcell Szegedi <szegedi.marcell@gmail.com>
2026-05-15 02:09:14 +0200

    refactor preparation script

--- prepare_testing.sh
+++ prepare_testing.sh
@@ -1,25 +1,63 @@
 #!/bin/bash
 
+###########################
+###     FLAG SYSTEM     ###
+###########################
+# r: Rebuild CPU binary
+# c: Run CPU Job
+# b: Run Base FPGA Job
+# o: Run Optimised FPGA Job
+
+REMAKE_CPU=false
+RUN_CPU=false
+RUN_FPGA_BASE=false
+RUN_FPGA_OPT=false
+while getopts "rcbo" opt; do
+    case $opt in
+        r) REMAKE_CPU=true ;;
+        c) RUN_CPU=true ;;
+        b) RUN_FPGA=true ;;
+        o) RUN_FPGA_OPT=true ;;
+        *) usage ;;
+    esac
+done
+
+if [[ $OPTIND -eq 1 ]]; then usage; fi
+
 set -e
 
-make_cpu_flood=true
-
 CPU_DIR="cpu_impl"
+CPU_BINARY="flood"
 
-cd "$CPU_DIR"
-if [[ "$make_cpu_flood" == true || ! -x "$CPU_BINARY" ]]; then
-    echo "Building CPU flood binary..."
-    rm flood
+# CPU binary building
+if [[ "$REMAKE_CPU" == true ]]; then
+    echo "Rebuilding CPU flood binary ..."
+    cd "$CPU_DIR"
+    make clean  # Cleaner than rm
     make flood
-else
-    echo "Using existing CPU flood binary."
+    cd ..
+elif [[ ! -x "$CPU_DIR/$CPU_BINARY" ]]; then
+    echo "CPU flood binary missing. Building now..."
+    cd "$CPU_DIR" && make flood && cd ..
 fi
 
-echo "Submitting CPU job..."
-CPU_JOB_ID=$(sbatch --parsable job.sh)
-echo "CPU job submitted with ID: ${CPU_JOB_ID}"
-cd ..
+# CPU job launch
+if [[ "$RUN_CPU" == true ]]; then
+    echo "Submitting CPU job..."
+    cd "$CPU_DIR"
+    CPU_JOB_ID=$(sbatch --parsable job.sh)
+    echo "CPU job submitted with ID: ${CPU_JOB_ID}"
+    cd ..
+fi
 
-echo "Submitting FPGA job..."
-FPGA_JOB_ID=$(sbatch --parsable job_base.sh)
-echo "FPGA job submitted with ID: ${FPGA_JOB_ID}"
+# Base FPGA job launch
+if [[ "$RUN_FPGA" == true ]]; then
+    echo "Submitting Base FPGA job..."
+    FPGA_JOB_ID=$(sbatch --parsable job_base.sh)
+    echo "Base FPGA job submitted with ID: ${FPGA_JOB_ID}"
+fi
+
+# Optimised FPGA job launch
+if [[ "$RUN_FPGA" == true ]]; then
+    echo "NOT Implemented Yet"
+fi
